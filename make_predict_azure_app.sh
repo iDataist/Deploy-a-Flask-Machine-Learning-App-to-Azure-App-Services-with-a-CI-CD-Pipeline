7d0e941bc85a448f1cb2040b4b97009e57b3f4f0
Hui Ren <huiren@Huis-MacBook-Pro.local>
2021-05-01 20:05:01 -0500

    Update make_predict_azure_app.sh

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://<yourappname>.azurewebsites.net:$PORT/predict 
+     -X POST https://webapp20210501.azurewebsites.net:$PORT/predict 
      #your application name <yourappname>goes here
