ebacbd00cd62c1ec3f8d62c3d1cf1de6773d1b1a
Hui Ren <huiren16@gmail.com>
2021-06-05 13:07:01 -0500

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,4 +3,4 @@ from app import app
 def test_home():
     response = app.test_client().get('/')
     assert response.status_code == 200
-    assert response.data == b'<h3>Sklearn Prediction Home</h3>'
+    assert response.data == b'<h3>Credit Card Fraud Detection Home</h3>'
