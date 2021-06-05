43b23eddc17f88250f5c12cf6b198cc812126cba
Hui Ren <huiren16@gmail.com>
2021-06-05 14:58:14 -0500

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -2,12 +2,6 @@
 ssh-keygen -t rsa
 cat /home/hui/.ssh/id_rsa.pub
 
-# Create a virtual environment¶
-conda create -n py35 -c free scikit-learn=0.20.3
-conda activate py35
-conda install -c anaconda flask==1.0.2
-conda install -c conda-forge pandas==0.24.2
-
 # Run test with locust
 python3 -m venv .locust
 source .locust/bin/activate
@@ -16,7 +10,7 @@ locust
 # http://localhost:8089
 
 # Deploy the webapp to Azure
-az webapp up -g group20210501 -n webapp20210501 --sku F1
+az webapp up -g group20210605 -n credit-card-fraud-detection --sku F1
 
 # Obtain credentials to connect Azure Pipeline and Azure Cloud Services 
 az ad sp create-for-rbac --query "{ client_id: appId, client_secret: password, tenant_id: tenant }"
